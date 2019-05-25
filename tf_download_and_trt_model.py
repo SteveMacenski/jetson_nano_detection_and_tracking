fa115924f9335ef4e24a68830447f90326a36724
Tyler Gragg <robots@tylergragg.com>
2019-05-25 17:42:51 -0400

    fixed broken download python import

--- tf_download_and_trt_model.py
+++ tf_download_and_trt_model.py
@@ -1,7 +1,7 @@
 import tensorflow.contrib.tensorrt as trt
 import sys
 import os
-from tf_trt_models.tf_trt_models.detection import download_detection_model, build_detection_graph
+from tf_trt_models.detection import download_detection_model, build_detection_graph
 
 # Options in model zoo: ssd_inception_v2_coco, ssd_mobilenet_v2_coco, ssd_mobilenet_v1_coco, ssdlite_mobilenet_v2_coco, ssd_mobilenet_v2_quantized_coco
 
